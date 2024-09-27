2bd43d2d8f189829289926cd9e5dec251bf9f280
SanskritiGusain <sanskritigusain050@gmail.com>
2024-09-27 14:18:17 +0530

    Add solution for valid parentheses problem and tests

--- program1.js
+++ program1.js
@@ -3,9 +3,34 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+
+    let stack = [];
     
+
+    const map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+    
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+       
+        if (map[char]) {
+            stack.push(char);
+        } else {
+        
+            let top = stack.pop();
+            if (map[top] !== char) {
+                return false;
+            }
+        }
+    }
+    
+   
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
